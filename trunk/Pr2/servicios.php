2c3a7aea11d7e88c97d96daf72cb22433939fb2f
jorge.shortys@gmail.com <jorge.shortys@gmail.com@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-04-07 23:29:51 +0000

    He hecho un nuevo menu desplegable para esta parte...me quedan quitar cosas del Css. Opiniones?

--- trunk/Pr2/servicios.php
+++ trunk/Pr2/servicios.php
@@ -8,10 +8,14 @@
 		<meta name="viewport" content="initial-scale=1">
 
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/bootstrap.min.css">
+		
 		<link rel="stylesheet" href="css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/templatemo_misc.css">
-		<link rel="stylesheet" href="css/desplegable.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/templatemo_style.css">
+		<link rel="stylesheet" href="css/menu.css">
+		
+		
 	</head>
 	<body>
 		<?php include "sidebar.php"
@@ -41,12 +45,47 @@
 				<!-- /#services -->
 			</div>
 			<!-- /#templatemo"-->
-			<p>
-				Categorias:
-			</p>
+			<p>Categorias:</p>
 			
+			<!--NUEVO -->
+			<div class="container">
+
+				<ul id="nav">
+					<li>
+						<a href="#">-Categoria 1</a>
+					</li>
+					<li>
+						<a href="#" class="sub" tabindex="1">-Categoria 2</a>
+						<ul>
+							<li>
+								<a href="#">->Servicio 1</a>
+							</li>
+							<li>
+								<a href="#">->Servicio 2</a>
+							</li>
+							<li>
+								<a href="#">->Servicio 3</a>
+							</li>
+							<li>
+								<a href="#">->Servicio 4</a>
+							</li>
+							<li>
+								<a href="#">->Servicio 5</a>
+							</li>
+						
+						</ul>
+						
+					</li>
+					<li>
+						<a href="#">-Categoria 3</a>
+					</li>
+					
+					
+				</ul>
+
+			</div>
 			
 		</div>
 		<!-- /#main-content-->
-
+		
 	</body>
