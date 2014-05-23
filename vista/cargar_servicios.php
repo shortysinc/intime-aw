9c28b959a33e59206b07bb656085f87e1588b34f
anguerrero@ucm.es <anguerrero@ucm.es@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-05-23 13:03:45 +0000

    categoria1.php mostrando servicios

--- vista/cargar_servicios.php
+++ vista/cargar_servicios.php
@@ -3,15 +3,22 @@
 	$categoria = $_GET['categoria'];
 	
 	if(isset($categoria)){
-		echo $categoria;
 		$BDD = new Mysql();
 		$servicios = $BDD->conseguirServiciosCategoria($categoria);
+
 		while($row = $servicios->fetch_assoc()){
-?>			<ul>
-				<li id="lista-servicios">
+?>			
+			<tr class "tabla-servicios">
+			 	<td class="columna-foto">
+					<a class="descripcion-servicios" href="trabajo.php"><img src="images/slide3.jpg" alt="Aqui iria una imagen"/></a>
+				</td>
+				<td class="columna-servicios">
 					<h3><?php echo $row['nombre_servicio'] ?></h3>
-				</li>
-			</ul>
+					<p><?php echo $row['descripcion'] ?></p>
+					<span id="nota-servicio">9.8</span>
+					<span id="horas-servicio">Horas: </span>
+				</td>
+			</tr>
 <?php
 		}
 	}
