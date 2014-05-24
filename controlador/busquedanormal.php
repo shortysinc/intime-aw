3a371295d43d208c1ce7ecd50fd85f90df174196
epelayo@ucm.es <epelayo@ucm.es@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-05-24 18:29:56 +0000

    Busqueda avanzada propia y por red de confianza implementadas.

--- controlador/busquedanormal.php
+++ controlador/busquedanormal.php
@@ -1,10 +1,33 @@
 <?php
 	require_once '../controlador/opbasededatos.php';
-	if (isset($_POST["nombrebusq"]) && $_POST["nombrebusq"]!=""){
-	$nbusq = $_POST["nombrebusq"];
-		$ret=array();
-		$BDD = new Mysql();
-		$ret=$BDD->busqueda($nbusq);
+	require_once '../modelo/usuario.php';
+	if ((isset($_POST["nombrebusq"]) && $_POST["nombrebusq"]!="") || (isset($_POST["consulta"]))){
+		if (isset($_POST["nombrebusq"])){
+			$nbusq = $_POST["nombrebusq"];
+			$ret=array();
+			$BDD = new Mysql();
+			$ret=$BDD->busqueda($nbusq);
+		}
+		if (isset($_POST["consulta"])){
+			session_start();
+			$usuario=$_SESSION['usuario'];
+			$consulta = $_POST["consulta"];
+			$corte = $_POST["valbusq"];
+			$ret=array();
+			$BDD = new Mysql();
+			$ret=$BDD->busquedaavanzada($corte,$usuario->getId());
+			if ($_POST["consulta"]=="red"){
+					$iteracionarray=array();
+					$sumaarray=array();
+					$sumaarray=$BDD->busquedaavanzada($corte,$ret[0][5]);
+					for ($i=1;$i<count($ret);$i=$i+1){
+						$iteracionarray=$BDD->busquedaavanzada($corte,$ret[$i][5]);
+						$sumaarray=array_merge((array)$sumaarray,(array)$iteracionarray);
+					}
+					$ret=array();
+					$ret=$sumaarray;
+			}
+		}
 		$lenght=count($ret);
 		for ($i=0;$i<$lenght;$i=$i+1){?>
 				<div class ="busq-ej">
@@ -22,8 +45,12 @@
 					</div>
 					<div class="busq-nota">
 						<?php
-							$nota=$BDD->notamedia($ret[$i][6]);
-							echo"<p>".$nota."</p>";
+							if (isset($_POST["nombrebusq"])){
+								$nota=$BDD->notamedia($ret[$i][6]);
+								echo"<p>".$nota."</p>";
+							}else{
+								echo"<p>Nota: ".$ret[$i][7]."</p>";
+							}
 						?>
 					</div>
 					<div class="busq-desc">
