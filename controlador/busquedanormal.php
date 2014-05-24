6d7a052e2a94bb9e3d8f1148c9f6164c71f9d69a
epelayo@ucm.es <epelayo@ucm.es@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-05-24 13:47:38 +0000

    busqueda v2

--- controlador/busquedanormal.php
+++ controlador/busquedanormal.php
@@ -1,32 +1,37 @@
 <?php
 	require_once '../controlador/opbasededatos.php';
+	if (!empty($_POST["nombrebusq"])){
 	$nbusq = $_POST["nombrebusq"];
-	$ret=array();
-	$BDD = new Mysql();
-	$ret=$BDD->busqueda($nbusq);
-	$lenght=count($ret);
-	for ($i=0;$i<$lenght;$i=$i+1){?>
-			<div class ="busq-ej">
-				<?php
-					echo"<a href=trabajo.php><h3>".$ret[$i][3]."</h3></a>";
-					echo"<a href=perfil.php><h4>".$ret[$i][0]." ".$ret[$i][1]."</h4></a>";
-				?>
-				<div class="busq-foto">
+		$ret=array();
+		$BDD = new Mysql();
+		$ret=$BDD->busqueda($nbusq);
+		$lenght=count($ret);
+		for ($i=0;$i<$lenght;$i=$i+1){?>
+				<div class ="busq-ej">
 					<?php
-						echo '<img src="data:image/png;base64,' . base64_encode($ret[$i][2]) . '"/>'
+						echo"<a href=trabajo.php><h3>".$ret[$i][3]."</h3></a>";
+						echo"<a href=perfil.php><h4>".$ret[$i][0]." ".$ret[$i][1]."</h4></a>";
 					?>
-				</div>
-				<div class="busq-nota">
-					<?php
-						$nota=$BDD->notamedia($ret[$i][6]);
-						echo"<p>".$nota."</p>";
-					?>
-				</div>
-				<div class="busq-desc">
-					<?php
+					<div class="busq-foto">
+						<?php
+							if (!empty($ret[$i][2])){
+								echo '<img src="data:image/png;base64,' . base64_encode($ret[$i][2]) . '"/>';
+							}else
+								echo '<img src="images/user.png">';
+						?>
+					</div>
+					<div class="busq-nota">
+						<?php
+							$nota=$BDD->notamedia($ret[$i][6]);
+							echo"<p>".$nota."</p>";
+						?>
+					</div>
+					<div class="busq-desc">
+						<?php
 						echo"<p>".$ret[$i][4]."</p>";
-					?>
+						?>
+					</div>
 				</div>
-			</div>
-	<?php
+		<?php
+		}
 	}
