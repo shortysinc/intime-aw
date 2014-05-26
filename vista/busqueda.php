332db348a43ec949446979073bb44da2c18b2604
epelayo@ucm.es <epelayo@ucm.es@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-05-26 09:11:34 +0000

    ahora los no registrados no ven la busqueda avanzada

--- vista/busqueda.php
+++ vista/busqueda.php
@@ -32,26 +32,28 @@
 												Enviar
 											</button>
 										</form>
-										<div class="avanzada">
-											<form method="post" action="busqueda.php" accept-charset="utf-8">
-												<label>Busqueda por confianza: </label>
-												<input type="radio" name="consulta" value="propia" checked="checked" />
-												Busca por confianza propia.
-												<input type="radio" name="consulta" value="red" />
-												Busca por red de confianza
-												<select name="valbusq">
-													<option value="5" selected>5</option>
-													<option value="6">6</option>
-													<option value="7">7</option>
-													<option value="8">8</option>
-													<option value="9">9</option>
-													<option value="10">10</option>
-												</select>
-												<button type="submit" value="Entrar">
-													Entrar
-												</button>
-											</form>
-										</div>
+										<?php if (isset($_SESSION['usuario'])){ ?>
+											<div class="avanzada">
+												<form method="post" action="busqueda.php" accept-charset="utf-8">
+													<label>Busqueda por confianza: </label>
+													<input type="radio" name="consulta" value="propia" checked="checked" />
+													Busca por confianza propia.
+													<input type="radio" name="consulta" value="red" />
+													Busca por red de confianza
+													<select name="valbusq">
+														<option value="5" selected>5</option>
+														<option value="6">6</option>
+														<option value="7">7</option>
+														<option value="8">8</option>
+														<option value="9">9</option>
+														<option value="10">10</option>
+													</select>
+													<button type="submit" value="Entrar">
+														Entrar
+													</button>
+												</form>
+											</div>
+										<?php } ?>
 								</div>
 							</div>
 						</div>
