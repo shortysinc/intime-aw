82bfcaee0d9fdf43d4382913fa06a0e7da249b36
anguerrero@ucm.es <anguerrero@ucm.es@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-05-27 20:02:54 +0000

    Mejoras en el diseño de la tabla de servicios y borrado de algunos archivos que no servían

--- vista/cargar_servicios.php
+++ vista/cargar_servicios.php
@@ -5,23 +5,28 @@
 	if(isset($categoria)){
 		$BDD = new Mysql();
 		$servicios = $BDD->conseguirServiciosCategoria($categoria);
-		
+?>
+		<table id = "tabla-servicios">
+<?php
 		while($row = $servicios->fetch_assoc()){
-			echo "entro en el bucle";
-?>			
-			<tr class "tabla-servicios">
+?>	
+			<tr class "fila-servicios">
 			 	<td class="columna-foto">
 					<a class="descripcion-servicios" href="trabajo.php"><img src="images/slide3.jpg" alt="Aqui iria una imagen"/></a>
 				</td>
 				<td class="columna-servicios">
 					<h3><?php echo $row['nombre_servicio'] ?></h3>
 					<p><?php echo $row['descripcion'] ?></p>
-					<span id="nota-servicio">9.8</span>
-					<span id="horas-servicio">Horas: </span>
+					<span id="nota-servicio">Nota: <span id="negrita">9.8</span></span>
+					<span id="horas-servicio"><span id="negrita">1 h</span></span>
 				</td>
 			</tr>
+				
 <?php
 		}
+?>
+		</table>
+<?php
 	}
 ?>
 	
