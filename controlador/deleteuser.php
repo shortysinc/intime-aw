7c2de51fdbe458cdf0e581ac08dfb2dcb63c779f
anguerrero@ucm.es <anguerrero@ucm.es@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-05-27 10:48:16 +0000

    clase Mysql ahora en op_base_datos.php y añadida una clase op_base_datos_usuario.php que contiene la clase MysqlUsuario que extiende de Mysql para hacer las consultas relativas de usuario. Lo suyo sería hacer esto para todas las tablas de la bdd

--- controlador/deleteuser.php
+++ controlador/deleteuser.php
@@ -6,6 +6,6 @@ $enlace = mysqli_connect('localhost', 'root', '', 'intime');
 
 $query = "DELETE FROM usuario WHERE id_usuario = '$id'";
 mysqli_query($enlace, $query);
-header('location:../vista/usuariosadmin.php');
- ?>
- 
+header('location:../vista/usuariosadmin.php'); 
+
+?>
