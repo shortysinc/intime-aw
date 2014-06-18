23b3a5c4857b68f57d655c07088223c878d56639
hector3k@gmail.com <hector3k@gmail.com@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-06-18 15:58:56 +0000

    include del header

--- vista/perfil_admin.php
+++ vista/perfil_admin.php
@@ -5,19 +5,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<title>inTime</title>
-		<meta name="keywords" content="sonic, responsive, free template, fluid layout, bootstrap, templatemo" />
-		<meta name="description" content="Sonic is one-page responsive free template using Bootstrap. This layout is suitable for creative portfolio showcase." />
-		<meta charset="utf-8">
-		<meta name="viewport" content="initial-scale=1">
-
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/templatemo_misc.css">
-		<link rel="stylesheet" href="css/templatemo_style.css">
-	</head>
+	<?php include "head.php"?>
 	<?php
 		if(!isset($_SESSION['login_admin']) || !$_SESSION['login_admin']){
 			$_SESSION["mensaje"] = "No tienes suficientes permisos";
--- vista/perfil_usuario.php
+++ vista/perfil_usuario.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="es">
 <?php 
 	require_once '../controlador/op_base_datos_usuario.php';
 	require_once '../controlador/op_base_datos_servicio.php';
@@ -10,22 +12,7 @@
 	require_once '../controlador/comprobar_login.php';
 	
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-	<head>
-		<title>inTime</title>
-		<meta name="keywords" content="sonic, responsive, free template, fluid layout, bootstrap, templatemo" />
-		<meta name="description" content="Sonic is one-page responsive free template using Bootstrap. This layout is suitable for creative portfolio showcase." />
-		<meta charset="utf-8">
-		<meta name="viewport" content="initial-scale=1">
-
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/templatemo_misc.css">
-		<link rel="stylesheet" href="css/templatemo_style.css">
-	</head>
+	<?php include "head.php"?>
 	<script src="jquery/jquery-2.1.0.min.js" type="text/javascript"></script>
 	<body>
 		<?php include "sidebar.php";
