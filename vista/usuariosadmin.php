de99afc2884b6da8a0609a12ad6a3dec4d6f290d
hector3k@gmail.com <hector3k@gmail.com@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-06-18 16:03:25 +0000

    Se me habian olvidado 2

--- vista/usuariosadmin.php
+++ vista/usuariosadmin.php
@@ -6,26 +6,7 @@
 	session_start();
 ?>
 <html lang="es">
-	<head>
-		<title>inTime</title>
-		<meta name="keywords" content="sonic, responsive, free template, fluid layout, bootstrap, templatemo" />
-		<meta name="description" content="Sonic is one-page responsive free template using Bootstrap. This layout is suitable for creative portfolio showcase." />
-		<meta charset="utf-8">
-		<meta name="viewport" content="initial-scale=1">
-
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/templatemo_misc.css">
-		<link rel="stylesheet" href="css/templatemo_style.css">
-		<script type="text/javascript">
- 	     function ConfirmDelete(ID)
-  		    {
-        	    if (confirm("¿Seguro que desea borrar el Usuario " + ID +" ?"))
-       	          location.href='../controlador/deleteuser.php?id=' + ID;
-  		    }
-		  </script>
-	</head>
+	<?php include "head.php";?>
 	<body>
 		<?php include "sidebar.php";
 		?>
@@ -81,4 +62,11 @@
 		</div>
 		<!-- /#main-content -->
 	</body>
+	<script type="text/javascript">
+ 	     function ConfirmDelete(ID)
+  		    {
+        	    if (confirm("¿Seguro que desea borrar el Usuario " + ID +" ?"))
+       	          location.href='../controlador/deleteuser.php?id=' + ID;
+  		    }
+		  </script>
 </html>
