5005c9311b7509061b70242abe4ab5c3cbd1b8f7
anguerrero@ucm.es <anguerrero@ucm.es@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-06-22 11:19:11 +0000

    Solamente se pueden responder solicitudes pendientes

--- trunk/controlador/enviar_respuesta.php
+++ trunk/controlador/enviar_respuesta.php
@@ -7,21 +7,29 @@ session_start();
 
 require_once "comprobar_login.php";
 
-if(isset($_POST['respuesta'], $_GET['id_solicitud'])){
+if (isset($_POST['respuesta'], $_GET['id_solicitud'])) {
 	$BDD = new MysqlRespuesta();
+	$BDDUsuario = new MysqlUsuario();
 	$usuario = $_SESSION['usuario'];
 	$respuesta = $_POST['respuesta'];
-	
-	$error = $BDD->insertarRespuesta($usuario->getId(), $_GET['id_solicitud'], $_POST['respuesta']);
-	if($error > 0){
-		$_SESSION['error'] = "Ha ocurrido un error al enviar la respuesta";
-	}else {
-		$_SESSION['mensaje'] = "Su respuesta ha sido enviada con éxito";
+
+	$solicitud = $BDDUsuario -> conseguirSolicitudPorId($_GET['id_solicitud']);
+	//Si es una solicitude que está pendiente
+	if ($solicitud -> getEstado() == 0) {
+		//La inserto
+		$error = $BDD -> insertarRespuesta($usuario -> getId(), $_GET['id_solicitud'], $_POST['respuesta']);
+
+		if ($error > 0) {
+			$_SESSION['error'] = "Ha ocurrido un error al enviar la respuesta";
+		} else {
+			$_SESSION['mensaje'] = "Su respuesta ha sido enviada con éxito";
+		}
+	}else{
+		$_SESSION['error'] = "Ya no se puede enviar la respuesta";
 	}
-	
-}else {
+
+} else {
 	$_SESSION['error'] = "Ha ocurrido un error al enviar la respuesta";
 }
 
 header('Location: ../vista/solicitudes.php');
-
