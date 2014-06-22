7589fa21e9907b5d525849ab49fb7a4a534ee7e9
anguerrero@ucm.es <anguerrero@ucm.es@00a6d866-901f-2c74-eb74-6c5c101226f6>
2014-06-22 01:18:44 +0000

    Cuando solicitas un servicio la fecha inicial se suma a las horas del servicio y se guardan. Corregidos algunos errores

--- trunk/controlador/createservice.php
+++ trunk/controlador/createservice.php
@@ -1,28 +1,35 @@
 <?php
-	require_once '../modelo/servicio.php';
-	require_once '../modelo/usuario.php';
-	require_once 'op_base_datos_servicio.php';
-	session_start();
-	require_once '../controlador/comprobar_login_usuario_admin.php';
-	
+require_once '../modelo/servicio.php';
+require_once '../modelo/usuario.php';
+require_once 'op_base_datos_servicio.php';
+session_start();
+
+require_once '../controlador/comprobar_login_usuario_admin.php';
+
+if (isset($_POST["nombreserv"], $_POST["descrpserv"], $_POST["categoria"], $_POST["horaserv"])) {
 	$nombre = $_POST["nombreserv"];
 	$descripcion = $_POST["descrpserv"];
-	$categoria = $_POST["categoria"]; 
+	$categoria = $_POST["categoria"];
 	$horas = $_POST["horaserv"];
-	$loginuser=$_SESSION['usuario'];
+	$loginuser = $_SESSION['usuario'];
 	
-	$BDDservice = new MysqlServicio();
-	$nombrecategoria=$BDDservice->conseguirCategoria($categoria);
+	//Quitar los decimales si los hubiera
+	$horas = floor ($horas);
 
-	if ($nombrecategoria!=NULL){
-	$id_servicio=$BDDservice->crearservicio($loginuser->getId(),$categoria,$nombre,$descripcion,$horas);
-	
-	if(isset($_FILES['foto'])&& !empty($_FILES['foto']["name"])){	
-		move_uploaded_file($_FILES['foto']['tmp_name'],"../vista/images/servicio/".$id_servicio.".png");
-		$foto=$id_servicio.".png";
-		$BDDservice->editarServicio($id_servicio,NULL,NULL,$foto);
+	$BDDservice = new MysqlServicio();
+	$nombrecategoria = $BDDservice -> conseguirCategoria($categoria);
+
+	if ($nombrecategoria != NULL) {
+		$id_servicio = $BDDservice -> crearservicio($loginuser -> getId(), $categoria, $nombre, $descripcion, $horas);
+
+		if (isset($_FILES['foto']) && !empty($_FILES['foto']["name"])) {
+			move_uploaded_file($_FILES['foto']['tmp_name'], "../vista/images/servicio/" . $id_servicio . ".png");
+			$foto = $id_servicio . ".png";
+			$BDDservice -> editarServicio($id_servicio, NULL, NULL, $foto);
+		}
+
+		header("location: ../vista/servicio.php?id_servicio=" . $id_servicio);
 	}
-	
-	header("location: ../vista/servicio.php?id_servicio=".$id_servicio);
-	}
-	else{ header("location: ../vista/crearservicio.php");}
+} else {
+	 header("location: ../vista/crearservicio.php");
+}
